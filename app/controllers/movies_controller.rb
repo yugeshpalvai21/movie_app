f0f9966a384d33d07e0902581be848dcefa61a62
Yugesh Palvai <pyreddy517@hotmail.com>
2020-02-25 20:05:03 +0530

    Implements Movies destroy action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -34,6 +34,12 @@ class MoviesController < ApplicationController
     end
   end
 
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path, notice: 'movie deleted successfully..'
+  end
+
   private
 
   def movie_params
