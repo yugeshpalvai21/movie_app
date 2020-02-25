5f6a18bd4fa1a56ff674f8f1fb30885e7b9ca952
Yugesh Palvai <pyreddy517@hotmail.com>
2020-02-25 14:34:10 +0530

    Seeding genres

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+genres = %w(Action Adventure Animation Biography Comedy Crime Documentary Drama Fantasy History Horror Musical Mystery Romance Sci-Fi Thriller War Western) 
+
+genres.each { |genre| Genre.create(name: genre) }
+
+puts "All Genres Created.."
