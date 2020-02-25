3325184d956252f41262159a9dc9ea799a5c7255
Yugesh Palvai <pyreddy517@hotmail.com>
2020-02-25 16:59:55 +0530

    Implements Movies new create index actions

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,25 @@
+class MoviesController < ApplicationController
+  
+  def index
+    @movies = Movie.all
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movies_path, notice: 'New Movie Successfully Created..'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :year, :director, :main_star, :description)
+  end
+end
