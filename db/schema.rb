a63f5e2becce97a912bfafa6424db200844f714c
Yugesh Palvai <pyreddy517@hotmail.com>
2020-02-26 14:35:54 +0530

    Implements n:n association between Movie and User

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_070118) do
+ActiveRecord::Schema.define(version: 2020_02_26_085444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movie_id"], name: "index_favorites_on_movie_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
@@ -43,4 +52,6 @@ ActiveRecord::Schema.define(version: 2020_02_26_070118) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "favorites", "movies"
+  add_foreign_key "favorites", "users"
 end
