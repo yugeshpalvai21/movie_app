29020abc5129d6d81abdea22b2771bfa9f7434ad
Yugesh Palvai <pyreddy517@hotmail.com>
2020-02-26 12:40:16 +0530

    User model generated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_105153) do
+ActiveRecord::Schema.define(version: 2020_02_26_070118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 2020_02_25_105153) do
     t.string "main_star"
     t.text "description"
     t.integer "favorited", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
